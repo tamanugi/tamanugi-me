4cd0bc264bd97a9fb2ab49eaffd8750b705c380d
tamanugi <tamanugi@gmail.com>
2018-11-12 20:42:47 +0900

    section.skill move to component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SkillComponent } from './section/skill/skill.component';
 import { ProfileComponent } from './section/profile/profile.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    SkillComponent,
     ProfileComponent
   ],
   imports: [
---
+++ src/app/section/skill/skill.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-skill',
+  templateUrl: './skill.component.pug',
+  styleUrls: ['./skill.component.styl']
+})
+export class SkillComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

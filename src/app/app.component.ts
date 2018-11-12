5ade09e866082ba8a498d1a542388a40c5f67214
tamanugi <tamanugi@gmail.com>
2018-11-12 20:25:41 +0900

    implements in-page scroll

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'tamanugi-me';
+
+  scroll(el) {
+    el.scrollIntoView({behavior: 'smooth', block: 'start'});
+  }
 }

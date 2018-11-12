f85e3bb357e97b3b20db77e21c0296ee965c3f93
tamanugi <tamanugi@gmail.com>
2018-11-12 20:35:18 +0900

    section.profile move to component

---
+++ src/app/section/profile/profile.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-profile',
+  templateUrl: './profile.component.pug',
+  styleUrls: ['./profile.component.styl']
+})
+export class ProfileComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
